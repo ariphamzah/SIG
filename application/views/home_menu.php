efd4cabce1d2773f9b03930efd29f571a8c22fca
ariphamzah <arifhamzah947>
2024-12-15 20:33:47 +0700

    coba

--- application/views/home_menu.php
+++ application/views/home_menu.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Dashboard</title>
+  <title>ARIP</title>
 
   <!-- PETA -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
